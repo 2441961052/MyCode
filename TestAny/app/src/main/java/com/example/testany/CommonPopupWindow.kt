dae850a9fb9abd8315bf4f4fdc916c6fc94da068
zhipingWang <zhiping.wang@fenrir-inc.com.cn>
2022-09-22 09:56:31 +0800

    NavigationView

--- TestAny/app/src/main/java/com/example/testany/CommonPopupWindow.kt
+++
@@ -1,5 +0,0 @@
-package com.example.testany
-
-class CommonPopupWindow {
-    
-}
--- TestLiveData/app/src/main/java/com/example/testlivedata/LiveDataViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testlivedata
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class LiveDataViewModel: ViewModel() {
-    var str = MutableLiveData<String>()
-}
